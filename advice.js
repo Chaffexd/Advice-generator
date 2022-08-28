1f5bab3d3c0a324e13f6a1ac1480fc68f45fd599
Shane Chaffe <67240269+Chaffexd@users.noreply.github.com>
2022-08-28 22:04:37 +0800

    Update
    - Added functionality to click for a quote on the dice icon
    - Works both ways, will generate a quote every 8 seconds or randomise one on click

--- advice.js
+++ advice.js
@@ -1,9 +1,11 @@
 const advice = document.getElementById("advice");
 const adviceNum = document.getElementById("adviceNum");
+const button = document.getElementById("getAdvice");
 
 setInterval(async function(){
     const response = await fetch(`https://api.adviceslip.com/advice?t=` + Math.random(), {cache: 'no-cache'});
     if(response.ok) {
+        
         const responseJson = await response.json();
         const data = responseJson.slip;
         console.log(data);
@@ -18,3 +20,22 @@ setInterval(async function(){
         throw new Error("Error on the call")
     }
   }, 8000);
+
+const getAdvice = async () => {
+    const response = await fetch(`https://api.adviceslip.com/advice?t=` + Math.random(), {cache: 'no-cache'});
+    
+    const responseJson = await response.json();
+    const data = responseJson.slip;
+    console.log(data);
+
+    const returnedAdvice = data.advice;
+    console.log(returnedAdvice);
+    const idAdvice = data.id;
+
+    adviceNum.innerText = `ADVICE #${idAdvice}`;
+    advice.innerText = `"${returnedAdvice}"`;
+}
+button.addEventListener("click", () => {
+    getAdvice();
+});
+
